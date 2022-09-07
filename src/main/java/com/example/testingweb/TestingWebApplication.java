3a65c586c5c28bc7baa711eebbaad50b6d99fb24
johnnysolizv <50997730+johnnysolizv@users.noreply.github.com>
2022-09-07 13:00:58 -0300

    🔒️ string password

--- src/main/java/com/example/testingweb/TestingWebApplication.java
+++ src/main/java/com/example/testingweb/TestingWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestingWebApplication {
-
+	String password = "password123";
 	public static void main(String[] args) {
 		SpringApplication.run(TestingWebApplication.class, args);
 	}
